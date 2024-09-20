4f959333f31479443c681f2b3df510ce8819b8cd
Omykronn <omykronn.player@gmail.com>
2024-09-20 09:41:05 +0200

    Ajout du code (v. TP2)

---
+++ TP-OBJET/src/main/java/org/centrale/objet/woe/tp/Lapin.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.woe.tp;
+
+/**
+ *
+ * @author grigm
+ */
+public class Lapin extends Monstre {
+    
+    public Lapin (int pV, int dA, int pPar, int paAtt, int paPar, Point2D p){
+        super(pV, dA, pPar, paAtt, paPar,p); 
+    }
+    
+    public Lapin(Lapin l){ 
+        super(l); 
+    }
+    
+    public Lapin() {
+        super();
+    }
+    
+}
