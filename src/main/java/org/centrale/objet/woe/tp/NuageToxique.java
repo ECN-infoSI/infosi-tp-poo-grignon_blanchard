964f51628ec24fd9ea4db3f523716f5448011e63
grigm <grigm@130.66.204.228>
2024-10-07 15:50:27 +0200

    Sous classe Objet NuageToxique + constructeur par défaut

---
+++ src/main/java/org/centrale/objet/woe/tp/NuageToxique.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.centrale.objet.woe.tp;
+
+/**
+ * Sous classe Objet représentant un nuage toxique
+ * @author grigm
+ */
+public class NuageToxique extends Objet {
+    /**
+     * Constructeur par défaut
+     * @author grigm
+     */
+    public NuageToxique(){
+        super(); 
+    }
+    
+}
